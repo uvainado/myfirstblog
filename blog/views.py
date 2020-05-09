3a8ffad36394f5199fb3324ba6ab9b87cb44aa90
uvainado <uvainado@gmail.com>
2020-05-09 19:53:58 +0900

    Added views to create/edit blog post inside the site.

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import redirect
-from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
 from django.shortcuts import render, get_object_or_404
